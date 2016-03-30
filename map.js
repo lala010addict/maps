5530c4fea5527ed08e4b58689366f1b446b9f152
jieyingpeng <jipeng@syr.edu>
2016-03-30 00:07:34 -0700

    displaying vendor ids

--- map.js
+++ map.js
@@ -1,23 +1,34 @@
-  function initialize() {
-      var center = new google.maps.LatLng(37.4419, -122.1419);
+function initialize() {
+    var center = new google.maps.LatLng(33.4419, -118.1419);
 
-      var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 3,
-          center: center,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-      });
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: center,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
 
-      var markers = [];
-      for (var i = 0; i < data.length; i++) {
-          var latLng = new google.maps.LatLng(data[i].latitude,
-              data[i].longitude);
-          console.log(data.latitude);
-          var marker = new google.maps.Marker({
-              position: latLng
-          });
-          markers.push(marker);
-      }
+    var markers = [];
+    for (var i = 0; i < data.length; i++) {
+        var latLng = new google.maps.LatLng(data[i].latitude,
+            data[i].longitude);
+        console.log(data.latitude);
+        var marker = new google.maps.Marker({
+            position: latLng
+        });
+        markers.push(marker);
+    }
 
-      var markerCluster = new MarkerClusterer(map, markers);
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
+    var markerCluster = new MarkerClusterer(map, markers);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+var uniqVendor = _.uniq(_.pluck(data, 'vendorId'));
+console.log(uniqVendor)
+
+var elements = [];
+for (var i = 0; i < uniqVendor.length; i++) {
+    elements.push($('<label class="checkbox-inline"><input type="checkbox" value="">' + uniqVendor[i] + ' </label>'));
+}
+
+$('#vendor').append(elements);
